b7c31c55ec4a7cadcd54e76cb2d9b96ac3041448
Vimedha Chaturvedi <chaturvedivimedha@gmail.com>
2026-05-28 21:31:04 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map1=new HashMap<>();
+        HashMap<Character, Integer> map2=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map1.put(ch, map1.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            map2.put(ch, map2.getOrDefault(ch,0)+1);
+        }
+        return map1.equals(map2);
+
+    }
+}
